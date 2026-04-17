a9580083327fb3fdba581e36885ac98bf83f2c44
damon <zhaorong@outlook.com>
2026-04-17 15:35:22 +0800

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/nerosoft/linkyou/LinkyouApplication.java
@@ -0,0 +1,13 @@
+package com.nerosoft.linkyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkyouApplication.class, args);
+	}
+
+}
