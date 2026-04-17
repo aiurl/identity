4aa4a1cbcc84310c143b394027512d9ac4622f81
damon <zhaorong@outlook.com>
2026-04-17 16:56:42 +0800

    Add security scheme configuration to Swagger for API documentation

--- src/main/java/com/nerosoft/linkyou/configs/SwaggerConfiguration.java
+++ src/main/java/com/nerosoft/linkyou/configs/SwaggerConfiguration.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfiguration implements WebMvcConfigurer {
@@ -20,7 +23,16 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
 
     @Bean
     public OpenAPI linkyouOpenApi() {
+        String schemeName = "BearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components().addSecuritySchemes(schemeName,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Linkyou Identity API")
                         .description("User authentication and identity service for Linkyou")
